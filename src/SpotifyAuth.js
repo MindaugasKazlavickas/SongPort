6b1e9fedfbca23f4e7f81496796358a63d09b7fe
Mindaugas <mindaugaskazlavickas@gmail.com>
2025-03-18 18:12:07 +0200

    added spotify app ID

--- src/SpotifyAuth.js
+++ src/SpotifyAuth.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const CLIENT_ID = "your_spotify_client_id";
+const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
 const REDIRECT_URI = "https://song-port.vercel.app/";
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 const RESPONSE_TYPE = "token";
